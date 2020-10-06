db62a6c679c77e9cefd149c1b4b429894c775254
Akaiju <jackbuchaniec@gmail.com>
2020-10-05 22:18:52 -0500

    Set up basic coroutine for contextualmessage showing/hiding

--- Assets/Scripts/ContextualMessageController.cs
+++ Assets/Scripts/ContextualMessageController.cs
@@ -8,18 +8,23 @@ public class ContextualMessageController : MonoBehaviour
     private CanvasGroup canvasGroup;
     private TMP_Text messageText;
 
-    // Start is called before the first frame update
     void Start()
     {
         canvasGroup = GetComponent<CanvasGroup>();
         messageText = GetComponent<TMP_Text>();
 
         canvasGroup.alpha = 0;
+
+        StartCoroutine (ShowMessage("HOWDY HO", 2));
     }
 
-    private void ShowMessage(string message, float duration)
+    private IEnumerator ShowMessage(string message, float duration)
     {
+        canvasGroup.alpha = 1;
         messageText.text = message;
+        //wait for duration
+        yield return new WaitForSeconds(duration);
+        canvasGroup.alpha = 0;
     }
     //// Update is called once per frame
     //void Update()
