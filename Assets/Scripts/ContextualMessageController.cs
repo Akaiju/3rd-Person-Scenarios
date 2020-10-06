e4acd8e60a71f7dcebc6cbdab72690fb56f52846
Akaiju <jackbuchaniec@gmail.com>
2020-10-05 21:02:24 -0500

    Created Contextual message controller script w ShowMessage function
    WIP!

---
+++ Assets/Scripts/ContextualMessageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ContextualMessageController : MonoBehaviour
+{
+    private CanvasGroup canvasGroup;
+    private TMP_Text messageText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        messageText = GetComponent<TMP_Text>();
+
+        canvasGroup.alpha = 0;
+    }
+
+    private void ShowMessage(string message, float duration)
+    {
+        messageText.text = message;
+    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
